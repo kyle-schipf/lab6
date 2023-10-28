48237828ae87af461e5d85716ca9270aee33968c
kyle-schipf <149012572+kyle-schipf@users.noreply.github.com>
2023-10-27 21:37:47 -0400

    Edited main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # lab 6 Kyle Schipf 10_25_23
 
 def encoder():
-    user_input = input("Enter a string of 8 numbers to encode: ")
+    user_input = input("Please enter your password to encode: ")
     if len(user_input) == 8:
         encode_pass = ''
         for num in user_input:
@@ -10,7 +10,7 @@ def encoder():
                 encode_pass = encode_pass + convert[-1]
             else:
                 encode_pass = encode_pass + str(int(num) + 3)
-        print(encode_pass)
+        print('Your password has been encoded and stored!')
         return encode_pass
 
 
@@ -20,10 +20,13 @@ def decoder(encode_pass):
 if __name__ == '__main__':
     menu = True
     while menu:
-        menu = input(f'{"Menu":^25}\n{"-"*25}\n1 to encode your password\n2 to decode your password\nq to quit program ')
+        print(f'{"Menu"}\n{"-"*13}\n1. Encode\n2. Decode\n3. Quit ')
+        menu = input('\nPlease enter an option: ')
         if menu == '1':
             encode_pass = encoder()
+            print(f'')
         elif menu == '2':
-            decoder(encode_pass)
-        elif menu == 'q':
+            decode_pass = decoder(encode_pass)
+            print(f'The encoded password is {decode_pass}, and the original password is {encode_pass}')
+        elif menu == '3':
             menu = False
